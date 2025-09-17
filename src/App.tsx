e7ae85d2ea64418cbc405e4cc481030c9a1754ff
krishnendu-2003 <skrishnendu115@gmail.com>
2025-09-18 02:42:51 +0530

    uncommented

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-// // src/App.tsx
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import WhitelistCheckerFollow from "./components/WhitelistCheckerFollow";
-// import WhitelistCheckerWallet from "./components/WhitelistCheckerWallet";
-
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<WhitelistCheckerFollow />} />
-//       <Route path="/wallet" element={<WhitelistCheckerWallet />} />
-//     </Routes>
-//   );
-// }
-
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import WhitelistCheckerFollow from "./components/WhitelistCheckerFollow";
